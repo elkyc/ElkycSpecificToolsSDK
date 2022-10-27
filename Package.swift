7c42eb47e0274b7791ddb56439d53879b681106e
Alex Gurin <oleksii.gurin@vivint.com>
2022-10-27 08:58:57 -0600

    version 2.1.3

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let version = "2.1.2"
-let checksum = "7196aee9028b3bf143d1fc80ddab7fa65cbd6de964a4c6e21f397987d2a9f682"
+let version = "2.1.3"
+let checksum = "79eac10fa4b56fb31d36ec11d7dcb3ef10f2819f2e432cb5da49604e7823a11f"
 
 let package = Package(
     name: "ElkycSpecificToolsSDK",
