3192e83a3de2c08884ef2e7b3edb04ad02554dc9
Sergey Chausov <chouzoff@gmail.com>
2023-05-31 09:03:26 +0300

    version 2.2.0

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let version = "2.1.5"
-let checksum = "34ce6d5d85d4de5f02f5f2a459ff8c0f2c95b661cc2af7c477a18947d0a107c1"
+let version = "2.2.0"
+let checksum = "da59025c446012341de3d2c415bbb6c6e6fac84c7f3af5b5c6557400c1236ff2"
 
 let package = Package(
     name: "ElkycSpecificToolsSDK",
